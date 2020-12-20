f9b84bac968cf8dd41e71d86d917a8d1ec5bf1a4
Miguel Pereira <mikasisto2@gmail.com>
2020-12-20 18:08:58 +0000

    Adicionado random presences

--- discord_bot.py
+++ discord_bot.py
@@ -2,15 +2,21 @@ import discord
 import database
 import os
 import utils_mikas
-from discord.ext import commands
+import presences
+from discord.ext import commands, tasks
 from dotenv import load_dotenv
 
-VERSION = '0.0.1.2'
+VERSION = '0.0.2'
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
+prefix = os.getenv('DISCORD_PREFIX')
+
+if prefix is None:
+    prefix = '!'
 
 
+# Singleton definition
 def singleton(cls, *args, **kw):
     instances = {}
     def _singleton():
@@ -23,13 +29,20 @@ def singleton(cls, *args, **kw):
 @singleton
 class DiscordBot(object):
     def __init__(self):
-        self.bot = commands.Bot(command_prefix='!')
+        self.bot = commands.Bot(command_prefix=prefix)
+
+# Fetch bot (singleton) and db
 
 bot = DiscordBot().bot
 DB = database.getDB()
 
+# Helper Functions
+
 def getBot():
     return DiscordBot().bot
+
+def start_bot():
+    bot.run(token)
 
 def getBindedChannel(ctx):
     rows = DB(DB.config.key == "bindedChannel" and DB.config.guild == ctx.guild.id).select()
@@ -37,6 +50,29 @@ def getBindedChannel(ctx):
         return bot.get_channel(int(rows[0].value))
     else:
         return None
+
+async def MangaDexNotification(name,image_url, url):
+    rows = DB(DB.config.key == "bindedChannel").select()
+    for row in rows:
+        channel_id = row.value
+        channel = bot.get_channel(channel_id)
+        embedVar = discord.Embed(title=name, description="New Chapter", url=url, color=0x00ff00)
+        embedVar.set_footer(text="Made by Mikas™")
+        embedVar.set_image(url=image_url)
+        embedVar.set_thumbnail(url=image_url)
+        await bot.get_channel(int(channel_id)).send(embed=embedVar)
+
+async def setRandomPresence():
+    presence = presences.getRandomPresence()
+    await bot.change_presence(status = presence.status, activity = presence.activity, afk = presence.afk)
+
+# Tasks 
+
+@tasks.loop(seconds=10)
+async def change_status():
+    await setRandomPresence()
+
+# Checks
 
 @bot.check
 async def globally_block_dms(ctx):
@@ -47,9 +83,12 @@ async def globally_check_channel(ctx):
     channel = getBindedChannel(ctx)
     return channel is None or ctx.command == bindChannel or channel == ctx.channel
 
+# Events
+
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
+    change_status.start()
 
 @bot.event
 async def on_message(message):
@@ -61,6 +100,13 @@ async def on_message(message):
         await message.channel.send(file=file)
     else:
         await bot.process_commands(message)
+
+@bot.event
+async def on_disconnect():
+    print("Disconnecting...")
+    # exit()
+
+# Commands
 
 @bot.command(name='teste', help='Mensagem de teste!')
 async def mensagemTeste(ctx):
@@ -87,22 +133,3 @@ async def bindChannel(ctx):
                            value=ctx.channel.id)
     DB.commit()                          
     await ctx.send('Agora só respondo neste channel! Haters!')
-
-async def MangaDexNotification(name,image_url, url):
-    rows = DB(DB.config.key == "bindedChannel").select()
-    for row in rows:
-        channel_id = row.value
-        channel = bot.get_channel(channel_id)
-        embedVar = discord.Embed(title=name, description="New Chapter", url=url, color=0x00ff00)
-        embedVar.set_footer(text="Made by Mikas™")
-        embedVar.set_image(url=image_url)
-        embedVar.set_thumbnail(url=image_url)
-        await bot.get_channel(int(channel_id)).send(embed=embedVar)
-
-@bot.event
-async def on_disconnect():
-    print("Disconnecting...")
-    # exit()
-
-def start_bot():
-    bot.run(token)
---
+++ presences.py
@@ -0,0 +1,37 @@
+import discord
+import random
+
+class RandomPresence:
+  def __init__(self, activity, status = discord.Status.online, afk = False):
+    self.activity = activity
+    self.status = status
+    self.afk = afk
+
+PRESENCES = [
+    #RandomPresence(activity=discord.Streaming(name="Ver Chads", url="https://discord.io/mikas")), # Testes de Streaming
+    #RandomPresence(activity=discord.CustomActivity(name="A ser refém de um orangotango")), # Testes de custom activity
+    RandomPresence(activity=discord.Game(name="Tenis numa mesa de pingue-pongo")),
+    RandomPresence(activity=discord.Game(name="O Ovo não é uma bola")),
+    RandomPresence(activity=discord.Game(name="Habbo")),
+    RandomPresence(activity=discord.Game(name="IMVU")),
+    RandomPresence(activity=discord.Game(name="VRChat")),
+    RandomPresence(activity=discord.Game(name="Olho?")),
+    RandomPresence(activity=discord.Game(name="Hasan = Chad")),
+    RandomPresence(activity=discord.Game(name="Half life 4")),
+    RandomPresence(activity=discord.Game(name="Bati num inglês e o meu pai pagou 2: eletric boogaloo")),
+    RandomPresence(activity=discord.Game(name="Há uma pinura de camões a piscar o olho")),
+    RandomPresence(activity=discord.Game(name="Tenho de gostar senão sou homofóbico né?")),
+    RandomPresence(activity=discord.Game(name="(...) ela quis beijar me e eu fugi para a casa de banho")),
+    RandomPresence(activity=discord.Game(name="uma gaja quer vir a minha casa mas não deixo porque ela quer trazer o cão")),
+    RandomPresence(activity=discord.Game(name="quem tem de tomar essas decisões não és tu, fazes parte do marketing")),
+    RandomPresence(activity=discord.Game(name="Eu prefiro dar o cú, se tu preferes dar as costas...")),
+    RandomPresence(activity=discord.Game(name="configuras-me o ts?")),
+    RandomPresence(activity=discord.Game(name="Para o ano venho de cosplay")),
+    RandomPresence(activity=discord.Game(name="É um cigano que eu conheci na rua")),
+    RandomPresence(activity=discord.Game(name="Ele tem cadelas, és só mais uma")),
+]
+
+def getRandomPresence():
+    return random.choice(PRESENCES)
+    
+    
